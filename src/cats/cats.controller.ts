76b7c6c56c4de9dacdb06a29148d6c648647c807
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2020-05-26 23:15:53 -0300

    :sparkles: cats: post route handler.

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,8 +1,13 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Req, Post } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
+    @Post()
+    create(): string {
+        return 'This action adds a new cat';
+    }
+
     @Get()
     findAll(@Req() request: Request): string {
         return 'This action returns all cats'
