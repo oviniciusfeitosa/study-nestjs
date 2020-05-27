f84ecc498b165d24c7dbd7bb6e977570422f598c
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2020-05-26 23:55:25 -0300

    :sparkles: admin: Sub-Domain Routing
    url: https://docs.nestjs.com/controllers#sub-domain-routing

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
+import { AdminController } from './admin/admin.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CatsController],
+  controllers: [AppController, CatsController, AdminController],
   providers: [AppService],
 })
 export class AppModule {}
