d944de2790acb3d94671f547c100f95ca65321ef
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2020-05-26 23:32:32 -0300

    :sparkles: cats: Status code

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Req, Post } from '@nestjs/common';
+import { Controller, Get, Req, Post, HttpCode } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
     @Post()
+    @HttpCode(204)
     create(): string {
         return 'This action adds a new cat';
     }
