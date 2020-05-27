149da0f9f29577ba707c4f81defc26045a7128f0
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2020-05-27 00:53:05 -0300

    :sparkles: Cats: Full resource sample
    reference: https://docs.nestjs.com/controllers#full-resource-sample

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,24 +1,34 @@
-import { Controller, Get, Req, Post, HttpCode, Param, Body } from '@nestjs/common';
-import { Request } from 'express';
-import { CreateCatDto } from './create-cat.dto';
+import { Controller, Get, Req, Post, HttpCode, Param, Body, Query, Put, Delete } from '@nestjs/common';
+import { CreateCatDto } from './dto/create-cat.dto';
+import { ListAllEntities } from './dto/list-all-entities.dto';
+import { UpdateCatDto } from './dto/update-cat.dto';
 
 @Controller('cats')
 export class CatsController {
     @Post()
-    @HttpCode(204)
-    async create(@Body() createCatDto: CreateCatDto) {
+    create(@Body() createCatDto: CreateCatDto): string {
         console.log(createCatDto);
-        return 'This action adds a new cat';
+        return `This action adds a new cat : { ${createCatDto.age}, ${createCatDto.name}, ${createCatDto.breed} }`;
     }
 
-    @Get('ab*cd')
-    findAll(@Req() request: Request): string {
+    @Get()
+    findAll(@Query() query: ListAllEntities): string {
         return 'This action returns all cats'
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+        return `This action update a #${id} cat`
     }
 
     @Get(':id')
     findOne(@Param() params): string {
         console.log(params.id);
         return `This action returns a #${params.id} cat`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `This action removes a #${id} cat`;
     }
 }
---
+++ src/cats/dto/list-all-entities.dto.ts
@@ -0,0 +1,6 @@
+export class ListAllEntities {
+    id: number;
+    name: string;
+    age: number;
+    breed: string;
+}
