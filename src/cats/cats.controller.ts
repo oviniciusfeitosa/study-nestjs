8d42a2d15577442091cf2c39407e089e2805bbb3
vinnyfs89 <viniciusfesil@gmail.com>
2020-05-29 09:54:10 -0300

    :alembic: cats: service dependency injection

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,34 +1,21 @@
 import { Controller, Get, Req, Post, HttpCode, Param, Body, Query, Put, Delete } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
-import { ListAllEntities } from './dto/list-all-entities.dto';
-import { UpdateCatDto } from './dto/update-cat.dto';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
 export class CatsController {
-    @Post()
-    create(@Body() createCatDto: CreateCatDto): string {
-        console.log(createCatDto);
-        return `This action adds a new cat : { ${createCatDto.age}, ${createCatDto.name}, ${createCatDto.breed} }`;
+    constructor(private catsService: CatsService){}
+
+   @Post()
+    async create(@Body() createCatDTO: CreateCatDto) {
+        this.catsService.create(createCatDTO);
     }
 
     @Get()
-    findAll(@Query() query: ListAllEntities): string {
-        return 'This action returns all cats'
+    async findAll(): Promise<Cat[]> {
+        return this.catsService.findAll();
     }
 
-    @Put(':id')
-    update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
-        return `This action update a #${id} cat`
-    }
-
-    @Get(':id')
-    findOne(@Param() params): string {
-        console.log(params.id);
-        return `This action returns a #${params.id} cat`;
-    }
-
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return `This action removes a #${id} cat`;
-    }
+   
 }
