7c4302c33fa9b3520419a3b67db4cd8adecbeb29
vinnyfs89 <viniciusfesil@gmail.com>
2020-05-28 23:27:22 -0300

    :sparkles: cats: Add Service Provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { AdminController } from './admin/admin.controller';
+import { CatsService } from './cats/cats.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CatsController, AdminController],
-  providers: [AppService],
+  providers: [AppService, CatsService],
 })
 export class AppModule {}
