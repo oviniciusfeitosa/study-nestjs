e3dad28d13f7c25bd7479806c0c26e03ebfebe02
vinnyfs89 <viniciusfesil@gmail.com>
2020-05-31 23:36:57 -0300

    :sparkles: cats : add module
    Co-authored-by: Ana Valente <anapfernandesc@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
 import { AdminController } from './admin/admin.controller';
-import { CatsService } from './cats/cats.service';
 import { HttpService } from './http/http.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController, AdminController],
-  providers: [AppService, CatsService, HttpService],
+  imports: [CatsModule],
+  controllers: [AppController, AdminController],
+  providers: [AppService, HttpService],
 })
 export class AppModule {}
