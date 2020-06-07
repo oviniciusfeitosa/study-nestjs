e01f635f85853fbe78f8b5157c27b3ae4e47f4d0
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2020-06-07 01:30:10 -0300

    :sparkles: Add Cats GraphQL Schema

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CatsResolver } from './cats.resolver';
 import { CatsController } from './cats.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Cat, CatSchema } from './cats.schemas';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
   providers: [CatsResolver],
   controllers: [CatsController],
 })
