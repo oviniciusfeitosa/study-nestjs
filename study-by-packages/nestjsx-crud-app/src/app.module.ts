9411e402dd71b4af36d5dfd8da545482cff3a0bb
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2020-11-20 04:59:51 -0300

    ✨ Simple crud

--- study-by-packages/nestjsx-crud-app/src/app.module.ts
+++ study-by-packages/nestjsx-crud-app/src/app.module.ts
@@ -1,9 +1,10 @@
+import { CompaniesModule } from './companies.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [CompaniesModule],
   controllers: [AppController],
   providers: [AppService],
 })
